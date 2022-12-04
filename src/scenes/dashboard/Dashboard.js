2339f4d796672526e4366c63e50f60a3b7a2df2a
James Serengia <jamesserengia@gmail.com>
2022-12-04 06:24:42 +0300

    Implemented sidebar menu

--- src/scenes/dashboard/Dashboard.js
+++ src/scenes/dashboard/Dashboard.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { useProSidebar } from "react-pro-sidebar";
 
 function Dashboard() {
-  const { collapseSidebar } = useProSidebar();
-
   return (
     <div>
-      <button onClick={() => collapseSidebar()}>Collapse</button>
       <h3>Dashboard</h3>
     </div>
   );
--- src/scenes/global/Sidebar.js
+++ src/scenes/global/Sidebar.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import {
+  Sidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  useProSidebar,
+} from "react-pro-sidebar";
 
-import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Avatar, Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
@@ -16,46 +22,164 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 
 function MySidebar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const { collapseSidebar, collapsed } = useProSidebar();
   return (
-    <Sidebar
-      //   backgroundColor="transparent"
-      backgroundColor={colors.primary[400]}
-      rootStyles={{
-        border: "solid 1px transparent",
-        color: "black",
-      }}
-    >
-      <Menu
+    <>
+      <Sidebar
+        //   backgroundColor="transparent"
+        backgroundColor={colors.primary[400]}
         rootStyles={{
-          backgroundColor: "transparent",
-          borderRight: "1px solid transparent",
-        }}
-        menuItemStyles={{
-          button: ({ level, active, disabled, hover }) => {
-            // only apply styles on first level elements of the tree
-            if (level === 0)
-              return {
-                color: disabled ? "#f5d9ff" : colors.grey[100],
-                backgroundColor: active ? "transparent" : undefined,
-                "&:hover": {
-                  backgroundColor: "#868efb3c",
-                },
-              };
-          },
+          border: "solid 1px transparent",
+          color: "black",
         }}
       >
-        <SubMenu label="Charts" rootStyles={{}}>
-          <MenuItem className={"menu-anchor"}> Pie charts </MenuItem>
-          <MenuItem className={"menu-anchor"}> Line charts </MenuItem>
-        </SubMenu>
-        <MenuItem>Documentation</MenuItem>
-        <MenuItem> Calendar </MenuItem>
-      </Menu>
-    </Sidebar>
+        <Box display="flex" alignItems="center" mt={2}>
+          {!collapsed && (
+            <Typography color={colors.grey[100]} variant="h5" ml="15px">
+              Admin
+            </Typography>
+          )}
+          <IconButton
+            sx={{
+              marginLeft: "auto",
+              marginRight: collapsed ? "auto" : "none",
+            }}
+          >
+            {collapsed ? (
+              <MenuOutlinedIcon onClick={() => collapseSidebar()} />
+            ) : (
+              <CloseOutlinedIcon onClick={() => collapseSidebar()} />
+            )}
+          </IconButton>
+        </Box>
+
+        {/* Avatar */}
+        {!collapsed && (
+          <Box textAlign="center">
+            <Box display="flex" alignItems={"center"} justifyContent="center">
+              <Avatar />
+            </Box>
+            <Typography
+              variant="h4"
+              color={colors.grey[100]}
+              sx={{ m: "10px 0 0 0" }}
+            >
+              James
+            </Typography>
+            <Typography variant="h5" color={colors.greenAccent[500]}>
+              Software Developer
+            </Typography>
+          </Box>
+        )}
+        <Menu
+          rootStyles={{
+            backgroundColor: "transparent",
+            borderRight: "1px solid transparent",
+          }}
+          menuItemStyles={{
+            button: ({ level, active, disabled, hover }) => {
+              // only apply styles on first level elements of the tree
+              if (level === 0)
+                return {
+                  color: disabled ? "#f5d9ff" : colors.grey[100],
+                  backgroundColor: active ? "transparent" : undefined,
+                  "&:hover": {
+                    backgroundColor: "#868efb3c",
+                  },
+                };
+            },
+          }}
+        >
+          <SubMenu
+            label="User Info"
+            rootStyles={{}}
+            icon={<PersonOutlinedIcon />}
+          >
+            <MenuItem
+              className={"menu-anchor"}
+              icon={<PersonOutlinedIcon />}
+              routerLink={<Link to="/profile" />}
+            >
+              {" "}
+              Profile{" "}
+            </MenuItem>
+            <MenuItem
+              className={"menu-anchor"}
+              routerLink={<Link to="/profile-settings" />}
+              icon={<SettingsOutlinedIcon />}
+            >
+              Account settings
+            </MenuItem>
+          </SubMenu>
+          <MenuItem
+            routerLink={<Link to="/dashboard" />}
+            icon={<HomeOutlinedIcon />}
+          >
+            Dashboard
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/team" />}
+            icon={<PeopleOutlinedIcon />}
+          >
+            Manage Team
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/contacts" />}
+            icon={<ContactsOutlinedIcon />}
+          >
+            Contact Information
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/invoices" />}
+            icon={<ReceiptOutlinedIcon />}
+          >
+            Invoices
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/calendar" />}
+            icon={<CalendarTodayOutlinedIcon />}
+          >
+            Calendar
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/faq" />}
+            icon={<HelpOutlineOutlinedIcon />}
+          >
+            FAQ
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/bar-chart" />}
+            icon={<BarChartOutlinedIcon />}
+          >
+            Bar Chart
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/pie-chart" />}
+            icon={<PieChartOutlineOutlinedIcon />}
+          >
+            Pie Chart
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/line-chart" />}
+            icon={<TimelineOutlinedIcon />}
+          >
+            Line Chart
+          </MenuItem>
+          <MenuItem
+            routerLink={<Link to="/geography-chart" />}
+            icon={<MapOutlinedIcon />}
+          >
+            Geography Chart
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </>
   );
 }
 
